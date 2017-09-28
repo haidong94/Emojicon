ead9352320b76414e7d901b5de15c8f480b3b8f0
haidong94 <haidong194@gmail.com>
2017-09-28 21:36:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dong/emojicon/TextWatcherAdapter.java
@@ -0,0 +1,25 @@
+package com.example.dong.emojicon;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by DONG on 28-Sep-17.
+ */
+
+public class TextWatcherAdapter implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
